cfe3f9a7f832de08ba85787bef7856759a4b4526
kretovEM <kretov.em@yandex.ru>
2019-09-27 17:00:12 +0300

    Реализация и регистрация сервисов Регистрация контестныйх данных

---
+++ MailSender/ViewModel/MainWindowViewModel.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using GalaSoft.MvvmLight;
+using MailSenderLib.Data.LinqToSql;
+using MailSenderLib.Services;
+
+namespace WpfTestMailSender.ViewModel
+{
+    public class MainWindowViewModel : ViewModelBase
+    {
+        private RecipientsDataProvider _recipientsDataProvider;
+
+        private string _WidowTitle = "Рассыльщик почты v0.1";
+
+        public string WidowTitle
+        {
+            get => _WidowTitle;
+            set => Set(ref _WidowTitle, value);
+        }
+
+        public ObservableCollection<Recipient> Recipients { get; } = new ObservableCollection<Recipient>();
+
+        public MainWindowViewModel(RecipientsDataProvider recipientsDataProvider)
+        {
+            _recipientsDataProvider = recipientsDataProvider;
+
+
+        }
+
+        private void RefreshData()
+        {
+            var recipients = Recipients;
+            recipients.Clear();
+            foreach (var recipient in _recipientsDataProvider.GetAll())
+            {
+                recipients.Add(recipient);
+            }
+        }
+    }
+}
