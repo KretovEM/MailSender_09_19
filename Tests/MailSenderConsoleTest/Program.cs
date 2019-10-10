b5fe8af9fe0ddbbbdec64cea7e39286d2d1ba221
kretov.em <kretov.em@yandex.ru>
2019-10-10 21:46:23 +0300

    Чистка кода, добавление EntityFraemwork 6 к консольному тестовому проекту

--- Tests/MailSenderConsoleTest/Program.cs
+++ Tests/MailSenderConsoleTest/Program.cs
@@ -12,14 +12,9 @@ namespace MailSenderConsoleTest
     {
         static void Main(string[] args)
         {
-            //TPLTests.Start();
-            //TaskTests.Start();
-            //AsyncAwaitTests.Start();
-            AsyncAwaitTests.StartAsync();
-
-            Console.WriteLine("Главный поток завершён!");
+            
             Console.ReadLine();
-            Console.WriteLine("Программа завершена...");
+            
         }
     }
 
