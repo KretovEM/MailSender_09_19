e7d78afa4f4d508288667202e011b82a17d2e2f1
kretov.em <kretov.em@yandex.ru>
2019-10-12 10:32:02 +0300

    Описание структуры БД - набор таблиц + Рефакторинг контекста БД

--- Tests/MailSenderConsoleTest/Data/SongsDB.cs
+++ Tests/MailSenderConsoleTest/Data/SongsDB.cs
@@ -1,7 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel.DataAnnotations;
-using System.ComponentModel.DataAnnotations.Schema;
+﻿using System.Collections.Generic;
 using System.Data.Entity;
 using System.Linq;
 using System.Text;
@@ -11,27 +8,7 @@ namespace MailSenderConsoleTest.Data
 {
     public class SongsDB : DbContext
     {
-
-    }
-
-    [Table("Track")]
-    public class Track
-    {
-        public int Id { get; set; }
-
-        [Required]
-        public string Name { get; set; }
-
-        public int Length { get; set; }
-    }
-
-    public class Artist
-    {
-        public int Id { get; set; }
-
-        [Required, MinLength(3)]
-        public string Name { get; set; }
-
-        public DateTime Birthday { get; set; }
+        public DbSet<Track> Tracks { get; set; }
+        public DbSet<Artist> Artists { get; set; }
     }
 }
---
+++ Tests/MailSenderConsoleTest/Data/Track.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace MailSenderConsoleTest.Data
+{
+    [Table("Track")]
+    public class Track
+    {
+        public int Id { get; set; }
+
+        [Required]
+        public string Name { get; set; }
+
+        public int Length { get; set; }
+    }
+}
